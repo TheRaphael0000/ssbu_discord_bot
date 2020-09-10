c2a7e5dc6addff76e537c59dfe64cd060206114c
Raphael Margueron <raphael.margueron@gmail.com>
2020-09-10 20:39:36 +0200

    random fighter

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ import discord
 from dotenv import load_dotenv
 
 from round_robin import round_robin, create_text
+from random_fighter import get_random_fighter
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
@@ -26,33 +26,35 @@ async def on_message(message):
 
     # Look up for message related to the bot
     words = re.split(r"\s+", message.content)
-    if words[0] != "!robin":
-        return
+    if words[0] == "!robin":
+        players = words[1:]
 
-    players = words[1:]
+        n = len(players)
 
-    n = len(players)
+        if n <= 0:
+            msg = """:
+    >>> **Create a round-robin tournament for 2 to 8 players**
+    *Syntax : !robin Player1 Player2 ... Player8*
+    GitHub: https://github.com/TheRaphael0000/round_robin_discord_bot
+    """
+            await message.channel.send(msg)
+            return
 
-    if n <= 0:
-        msg = """:
->>> **Create a round-robin tournament for 2 to 8 players**
-*Syntax : !robin Player1 Player2 ... Player8*
-GitHub: https://github.com/TheRaphael0000/round_robin_discord_bot
-"""
-        await message.channel.send(msg)
-        return
+        # Upper-bound
+        if n > 8 or n < 2:
+            await message.channel.send("*Please specify between 2 and 8 players*")
+            return
 
-    # Upper-bound
-    if n > 8 or n < 2:
-        await message.channel.send("*Please specify between 2 and 8 players*")
-        return
+        # Create round robin
+        r = round_robin(players)
+        # Create text message
+        t = create_text(n, r)
 
-    # Create round robin
-    r = round_robin(players)
-    # Create text message
-    t = create_text(n, r)
+        # Send it
+        await message.channel.send(t)
 
-    # Send it
-    await message.channel.send(t)
+    if words[0] == "!bravery":
+        fighter = get_random_fighter()
+        await message.channel.send(f"Your fighter is {fighter}")
 
 client.run(token)
---
+++ random_fighter.py
@@ -0,0 +1,6 @@
+fighters = ["Samus Sombre","Daisy","Chrom","Ken","Inkling","Ridley","Simon","Richter ","King K. Rool","Isabelle","Félinferno","Plante Piranha","Mario","Donkey Kong","Link","Samus","Yoshi","Kirby","Fox","Pikachu","Luigi","Ness","Captain Falcon","Rondoudou ","Peach","Bowser","Ice Climbers","Sheik","Zelda","Dr. Mario","Pichu","Falco","Marth","Lucina","Link Enfant","Ganondorf","Mewtwo","Roy","Mr. Game & Watch ","Meta Knight","Pit","Pit Maléfique ","Samus sans armure","Wario","Snake","Ike","Dresseur Pokémon","Diddy Kong","Lucas","Sonic","Roi Dadidou","Olimar","Lucario","R.O.B.","Link Cartoon","Wolf ","Villageois","Mega Man","Wii Fit Trainer","Harmonie & Luma","Little Mac","Amphinobi","Palutena","Pac-Man","Daraen","Shulk","Bowser Jr.","Duo Duck Hunt","Ryu","Cloud","Corrin","Bayonetta","Joker","Hero","Banjo & Kazooie","Terry","Byleth","Minmin"]
+
+import random
+
+def get_random_fighter():
+    return random.choice(fighters)
